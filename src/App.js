654575a5818b67073f118f4bcd9aabcbd1c8c623
Osama Ameer <osamaameer15@gmail.com>
2020-12-27 15:05:05 +0500

    queen animation
    projoject 4a useref, web animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Sky from './components/Sky';
+import Earth from './components/Earth';
+import Scenery from './components/Scenery';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+export default function App() {
+return (
+  <div>
+    <Sky />
+    <Earth />
+    <Scenery />
+  </div>
+  )
+
 }
-
-export default App;
